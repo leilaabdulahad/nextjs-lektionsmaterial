441becb0cda2749857bd3a85982de681dc537329
Leila <leilaabdulahad@hotmail.com>
2024-07-27 17:02:55 +0200

    fixed text in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <>
         <div className="relative max-w-screen-lg mx-auto text-center text-black p-20 bg-header-background bg-cover bg-center bg-no-repeat h-96">
             
-                    <p className="">Lektionsmaterial för högstadiet och gymnasiet gjorda av lärare för lärare.</p>                
+                    <p className="">Lektionsmaterial för högstadiet och gymnasiet skapade av lärare för lärare.</p>                
             <button className="button-gy absolute top-0 right-0 m-4">Gymnasiet</button>
             <button className="button-hs absolute top-20 right-0 m-4">Högstadiet</button>
         </div>
