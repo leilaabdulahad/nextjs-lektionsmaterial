b88e61834732847974832147feffd2615901560b
Leila <leilaabdulahad@hotmail.com>
2024-07-28 13:44:08 +0200

    minor changes to header and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,25 +5,24 @@ const Footer = () => {
   return (
     <footer className="text-white py-2 mt-20">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-        <div className="mb-4 md:mb-0">
-          <h2 className="text-2xl font-bold text-gray-400">LektionsMaterial</h2>
-          <p className="text-gray-400">För lärare av lärare</p>
+        <div>
+          <h2 className="text-2xl font-bold text-black mb-1">LektionsMaterial</h2>
         </div>
         <div className="flex space-x-4 mb-4 md:mb-0">
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-black">
             <FaFacebookF />
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-black">
             <FaTwitter />
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-black">
             <FaInstagram />
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-black">
             <FaLinkedinIn />
           </a>
         </div>
-        <div className="text-gray-400 text-sm">
+        <div className="text-black text-sm">
           &copy; {new Date().getFullYear()} LektionsMaterial. All rights reserved.
         </div>
       </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import { headerText } from "@/constants"
 function Header() {
     return (
         <div className="relative max-w-screen-lg mx-auto text-center text-black p-20 bg-header-background bg-cover bg-center bg-no-repeat h-96">
-            <p className="text-lg mt-5 font-medium text-white leading-relaxed max-w-xs mx-auto whitespace-pre-wrap">
+            <p className="text-lg mt-7 font-medium text-white leading-relaxed max-w-xs mx-auto whitespace-pre-wrap">
                 Lektionsmaterial för högstadiet och gymnasiet skapade av lärare för lärare.
             </p>
             <button className="button-gy absolute top-0 right-0 m-4">Gymnasiet</button>
