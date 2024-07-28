e50ca76472bcc31a1322253b4f17f5035c6e3c07
Leila <leilaabdulahad@hotmail.com>
2024-07-28 14:33:54 +0200

    adds card of employees and more info in about section

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,8 @@
-import { about } from '@/constants/about';
+import { about, employees } from '@/constants/about'
 
 const About = () => {
   return (
-    <div className="container mx-auto p-4 md:p-8 text-white ">
+    <div className="container mx-auto p-4 md:p-8">
       <img className="w-full h-64 object-cover rounded-md mb-8 shadow-lg bg-about-background" />
       <div className="flex flex-col items-center">
         {about.map((section, index) => (
@@ -11,9 +11,24 @@ const About = () => {
             <p className="text-lg text-gray-800">{section.content}</p>
           </div>
         ))}
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+          {employees.map((employee, index) => (
+            <div 
+              key={index} 
+              className="flex items-center p-5 bg-white rounded-lg shadow-md transform transition duration-500 ease-in-out hover:-translate-y-1"
+            >
+              <img src={employee.image} alt={employee.name} className="w-20 h-20 rounded-full mr-4" />
+              <div>
+                <h2 className="text-xl font-bold mb-2">{employee.name}</h2>
+                <a href={`mailto:${employee.email}`} className="text-gray-800 hover:underline">{employee.email}</a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
--- constants/about.js
+++ constants/about.js
@@ -1,6 +1,19 @@
 export const about = [
     {
         title: "Om LektionsMaterial",
-        content: "En samling högkvalitativ lektionsmaterial för årskurserna 6-9 i svenska, svenska som andraspråk och engelska. Vi strävar efter att erbjuda det bästa innehållet för att stödja dig i ditt arbete som lärare."
+        content: "En samling högkvalitativt lektionsmaterial för årskurserna 6-9 i svenska, svenska som andraspråk och engelska. Vi strävar efter att erbjuda det bästa innehållet för att stödja dig i ditt arbete som lärare. Vårt material är noggrant utvalt och anpassat för att möta de senaste läroplanerna och pedagogiska riktlinjerna. Genom att använda LektionsMaterial kan du vara säker på att du får tillgång till resurser som är både inspirerande och effektiva. Oavsett om du söker material för grammatik, litteraturstudier, skrivövningar eller språkutveckling, hittar du det hos oss. Vi är dedikerade till att kontinuerligt uppdatera och förbättra vårt utbud för att säkerställa att det alltid är relevant och användbart för både lärare och elever."
+    }
+]
+
+export const employees = [
+    {
+        name: "Leila Abdulahad",
+        email: "leila@lektionsmaterial.se",
+        image: "/employee.png"
+    },
+    {
+        name: "Tove Tvingstedt",
+        email: "tove@lektionsmaterial.se",
+        image: "/employee2.jpg"
     }
 ]
