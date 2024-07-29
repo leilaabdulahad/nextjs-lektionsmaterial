5372d89edc8fbc6cac463ca16c067ee15216476f
Leila <leilaabdulahad@hotmail.com>
2024-07-29 15:19:42 +0200

    adds footer at the bottom

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import { Inter } from "next/font/google"
 import ConvexClientProvider from "@/components/convex-client-provider"
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,10 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html className="bg-backgroundColor" lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <ConvexClientProvider>
           <Navbar />
+          <div className="flex-grow">
             {children}
+          </div>
+          <Footer />
         </ConvexClientProvider>
       </body>
     </html>
