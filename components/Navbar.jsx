ab24cc72dd2c6b1c5d8e71e416ec7726c5a3a7f9
Leila <leilaabdulahad@hotmail.com>
2024-11-04 21:09:55 +0100

    commented out the sign in button since regular users dont need to sign in, only admins, admins can access sign in other ways

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -64,9 +64,9 @@ const Navbar = () => {
                 </li>
               )
             })}
-            <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            {/* <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
               {user ? <UserButton /> : <SignInButton>Logga in</SignInButton>}
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
